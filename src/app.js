bc1ace472aed0d2fdf5788b4eb6ba517ee85738b
Aravind Mohanoor <aravindmohanoor@Aravinds-MacBook-Pro-2.local>
2020-03-29 00:16:22 +0530

    added highlighting for result snippet and method snippet

--- src/app.js
+++ src/app.js
@@ -46,13 +46,13 @@ const renderHits = (renderOptions, isFirstRender) => {
               <h3>${instantsearch.highlight({ attribute: 'title', hit: item })}</h3>
               <h5><i>${item.authors} - ${item.year} - DOI:<a target="_blank" href="${item.url}">${item.doi}</a></i></h5>
               <p>${instantsearch.highlight({ attribute: 'section_text', hit: item })}</p>
-              <div class="method_snippet">
-              <h5>${item.best_result_title}</h5>
-              <p>${item.best_result_snippet}</p>
-              </div>
               <div class="result_snippet">
+              <h5>${item.best_result_title}</h5>
+              <p>${instantsearch.highlight({ attribute: 'best_result_snippet', hit: item })}</p>
+              </div>
+              <div class="method_snippet">
               <h5>${item.best_method_title}</h5>
-              <p>${item.best_method_snippet}</p>
+              <p>${instantsearch.highlight({ attribute: 'best_method_snippet', hit: item })}</p>
               </div>
             </li>`
       
