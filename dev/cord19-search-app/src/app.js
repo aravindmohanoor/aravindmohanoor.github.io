f56f4c225daa341fe91645ea41fe0d66e34006e9
Aravind Mohanoor <aravindmohanoor@Aravinds-MacBook-Pro-2.local>
2020-03-31 19:05:20 +0530

    added dev folder with instantsearch template app

---
+++ dev/cord19-search-app/src/app.js
@@ -0,0 +1,51 @@
+/* global algoliasearch instantsearch */
+
+const searchClient = algoliasearch(
+  'ZZ2ZTTMSBH',
+  '71090d1229c06a4d72829a3d0d59d6bc'
+);
+
+const search = instantsearch({
+  indexName: 'papers_dev',
+  searchClient,
+});
+
+search.addWidgets([
+  instantsearch.widgets.searchBox({
+    container: '#searchbox',
+  }),
+  instantsearch.widgets.currentRefinements({
+    container: '#current-refinements',
+  }),
+
+  instantsearch.widgets.refinementList({
+    container: '#brand-list',
+    attribute: 'journal',
+  }),
+  instantsearch.widgets.configure({
+    hitsPerPage: 10,
+    facetingAfterDistinct: true,
+  }),
+  instantsearch.widgets.hits({
+    container: '#hits',
+    templates: {
+      item: `
+        <div>
+          <img src="{{image}}" align="left" alt="{{name}}" />
+          <div class="hit-name">
+            {{#helpers.highlight}}{ "attribute": "title" }{{/helpers.highlight}}
+          </div>
+          <div class="hit-description">
+            {{#helpers.highlight}}{ "attribute": "abstract_excerpt" }{{/helpers.highlight}}
+          </div>
+        </div>
+      `,
+    },
+  }),
+  instantsearch.widgets.pagination({
+    container: '#pagination',
+  }),
+]);
+
+
+search.start();
