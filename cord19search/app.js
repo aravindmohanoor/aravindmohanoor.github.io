75d6945c133aad53735215176bf1c65a417e809a
Aravind Mohanoor <aravindmohanoor@Aravinds-MacBook-Pro-2.local>
2020-04-14 20:04:34 +0530

    pushing files to prod

--- cord19search/app.js
+++ cord19search/app.js
@@ -1,23 +1,42 @@
 //Config
 var applicationID = 'ZZ2ZTTMSBH';
 var apiKey = '71090d1229c06a4d72829a3d0d59d6bc';
-var index = 'papers';
+var index = 'papers_dev';
 var hits_per_page = 10;
 
 var client = algoliasearch(applicationID, apiKey);
 var helper = algoliasearchHelper(client, index, {
-    disjunctiveFacets: ['journal','year'],
+    disjunctiveFacets: ['journal','year_month','design','outcome','difference'],
     facetingAfterDistinct: true,
     hitsPerPage: hits_per_page
 });
 
 window.displayAttributes = ['title','section_text','abstract_excerpt','best_method_snippet','best_result_snippet'];
 
+window.spanFilters = {}
+
 helper.on('result', function (content) {
     renderFacetList(content); // not implemented yet
     renderHits(content);
     updatePagination(content);
+    //highlightFilters();
+    highlightFilter('design');
+    highlightFilter('difference');
+    highlightFilter('outcome');
 });
+
+function highlightFilter(filterName){
+    $( "p.snippet" ).each(function( index ) {
+        highlighted_items = [];
+        $(this).find('.'+filterName).each(function( index ) {
+            var lower = $(this).text().toLowerCase();
+            if (!highlighted_items.includes(lower)){
+                $(this).addClass(filterName+'-pill');
+                highlighted_items.push(lower);
+            }
+        })
+    });
+}
 
 function updatePagination(content){
     var totalResults = content.nbHits;
@@ -41,9 +60,6 @@ function updatePagination(content){
     }
 }
 
-/*$('.dropdown-item').on('click',function () {
-    alert($(this).val());
-});*/
 
 function renderHits(content) {
     $('#container').html(function () {
@@ -63,74 +79,67 @@ function renderHits(content) {
             });
             liHit.append(pDetails);
             if (window.displayAttributes.indexOf('section_text') > -1){
-                var h5SnippetTitle = $('<h5/>');
+                var h6SnippetTitle = $('<h6/>');
                 var labelSnippetTitle = $('<span>',{
-                    html:hit.section
+                    html:'Snippet > '+hit.section
                 });
-                h5SnippetTitle.append(labelSnippetTitle);
-                labelSnippetTitle.addClass('badge badge-secondary');
+                h6SnippetTitle.append(labelSnippetTitle);
+                labelSnippetTitle.addClass('heading-pill');
                 strSectionText = '';
-                var sectionTextValue = hit._highlightResult.section_text;
-                if (Array.isArray(sectionTextValue)){
-                    for(var item in sectionTextValue){
-                        strSectionText += sectionTextValue[item].value + ' ';
-                    }
+                for (let [key, value] of Object.entries(hit._highlightResult.section_text)) {
+                    strSectionText += value.value+' ';
                 }
-                else{
-                    strSectionText = sectionTextValue;
-                }
-                // for(const text of hit._highlightResult.section_text){
-                //     strSectionText += text.value + ' ';
-                // }
                 var pSnippetText = $('<p>',{
                     html:strSectionText
                 });
-                liHit.append(h5SnippetTitle);
+                pSnippetText.addClass('snippet');
+                liHit.append(h6SnippetTitle);
                 liHit.append(pSnippetText);
             }
             if (window.displayAttributes.indexOf('abstract_excerpt') > -1){
                 if(hit.abstract_excerpt !== ''){
-                    var h5AbstractTitle = $('<h5/>');
+                    var h6AbstractTitle = $('<h6/>');
                     var labelAbstractTitle = $('<span>',{
                         html:'Abstract'
                     });
-                    h5AbstractTitle.append(labelAbstractTitle);
-                    labelAbstractTitle.addClass('badge badge-warning');
+                    h6AbstractTitle.append(labelAbstractTitle);
+                    labelAbstractTitle.addClass('heading-pill');
 
                     var pAbstract = $('<p>',{
                         html:hit._highlightResult.abstract_excerpt.value
                     });
-                    liHit.append(h5AbstractTitle);
+                    liHit.append(h6AbstractTitle);
                     liHit.append(pAbstract);
                 }
             }
             if (window.displayAttributes.indexOf('best_method_snippet') > -1){
                 if(hit.best_method_title !== ''){
-                    var h5MethodTitle = $('<h5/>');
+                    var h6MethodTitle = $('<h6/>');
                     var labelMethodTitle = $('<span>',{
-                        html:hit.best_method_title
+                        html:'Methods > '+hit.best_method_title
                     });
-                    h5MethodTitle.append(labelMethodTitle);
-                    labelMethodTitle.addClass('badge badge-success');
+                    h6MethodTitle.append(labelMethodTitle);
+                    labelMethodTitle.addClass('heading-pill');
+
                     var pMethodSnippet = $('<p>',{
                         html:hit._highlightResult.best_method_snippet.value
                     });
-                    liHit.append(h5MethodTitle);
+                    liHit.append(h6MethodTitle);
                     liHit.append(pMethodSnippet);
                 }
             }
             if (window.displayAttributes.indexOf('best_result_snippet') > -1){
                 if(hit.best_method_title !== ''){
-                    var h5ResultTitle = $('<h5/>');
+                    var h6ResultTitle = $('<h6/>');
                     var labelResultTitle = $('<span>',{
-                        html:hit.best_result_title
+                        html:'Results > '+hit.best_result_title
                     });
-                    h5ResultTitle.append(labelResultTitle);
-                    labelResultTitle.addClass('badge badge-primary');
+                    h6ResultTitle.append(labelResultTitle);
+                    labelResultTitle.addClass('heading-pill');
                     var pResultSnippet = $('<p>',{
                         html:hit._highlightResult.best_result_snippet.value
                     });
-                    liHit.append(h5ResultTitle);
+                    liHit.append(h6ResultTitle);
                     liHit.append(pResultSnippet);
                 }
             }
@@ -141,14 +150,58 @@ function renderHits(content) {
     });
 }
 
-$('#journal-facet').on('click', 'input[type=checkbox]', function (e) {
+function toggleFilterDisplay(facet, facetValue, facetLabel, isChecked){
+    if (isChecked){
+        var spanFilter = $('<span>',{
+            html:facetLabel+' : '+facetValue
+        });
+        spanFilter.addClass('badge badge-pill badge-secondary active-filter');
+        var buttonFilter = $('<i/>');
+        buttonFilter.addClass('btn btn-sm fa fa-close');
+        buttonFilter.attr('font-size','12px');
+        buttonFilter.attr('color','white');
+        buttonFilter.on('click',function (e) {
+            helper.toggleRefinement(facet, facetValue).search();
+            spanFilter.remove();
+        });
+        spanFilter.append(buttonFilter);
+        $('#spanFilters').append(spanFilter);
+        window.spanFilters[facetLabel+'_'+facetValue] = spanFilter;
+    }
+    else{
+        let filterElem = window.spanFilters[facetLabel+'_'+facetValue];
+        filterElem.remove();
+    }
+}
+
+$('#design-facet').on('click', 'input[type=checkbox]', function (e) {
     var facetValue = $(this).data('facet');
-    helper.toggleRefinement('journal', facetValue).search();
+    helper.toggleRefinement('design', facetValue).search();
+    toggleFilterDisplay('design',facetValue, 'Study Design', this.checked);
+});
+
+$('#outcome-facet').on('click', 'input[type=checkbox]', function (e) {
+    var facetValue = $(this).data('facet');
+    helper.toggleRefinement('outcome', facetValue).search();
+    toggleFilterDisplay('outcome',facetValue, 'Outcome', this.checked);
+});
+
+$('#difference-facet').on('click', 'input[type=checkbox]', function (e) {
+    var facetValue = $(this).data('facet');
+    helper.toggleRefinement('difference', facetValue).search();
+    toggleFilterDisplay('difference', facetValue, 'Risk factor', this.checked);
 });
 
 $('#year-facet').on('click', 'input[type=checkbox]', function (e) {
     var facetValue = $(this).data('facet');
-    helper.toggleRefinement('year', facetValue).search();
+    helper.toggleRefinement('year_month', facetValue).search();
+    toggleFilterDisplay('year_month',facetValue,'Month', this.checked);
+});
+
+$('#journal-facet').on('click', 'input[type=checkbox]', function (e) {
+    var facetValue = $(this).data('facet');
+    helper.toggleRefinement('journal', facetValue).search();
+    toggleFilterDisplay('journal',facetValue,'Journal', this.checked);
 });
 
 function renderFacetList(content) {
@@ -166,7 +219,48 @@ function renderFacetList(content) {
         });
     });
     $('#year-facet').html(function () {
-        return $.map(content.getFacetValues('year'), function (facet) {
+        allFacetValues = content.getFacetValues('year_month');
+        sortedFacetValues = allFacetValues.sort((a,b) => (b.name > a.name) ? 1 : ((a.name > b.name) ? -1 : 0));
+        return $.map(sortedFacetValues, function (facet) {
+            var checkbox = $('<input type=checkbox>').
+            data('facet', facet.name).
+            attr('id', 'fl-' + facet.name);
+            if (facet.isRefined) checkbox.attr('checked', 'checked');
+            var label = $('<label>').html(facet.name + ' (' + facet.count + ')').
+            attr('for', 'fl-' + facet.name);
+            return $('<li>').append(checkbox).append(label);
+        });
+    });
+    $('#design-facet').html(function () {
+        allFacetValues = content.getFacetValues('design');
+        sortedFacetValues = allFacetValues.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));
+        return $.map(sortedFacetValues, function (facet) {
+            var checkbox = $('<input type=checkbox>').
+            data('facet', facet.name).
+            attr('id', 'fl-' + facet.name);
+            if (facet.isRefined) checkbox.attr('checked', 'checked');
+            var label = $('<label>').html(facet.name + ' (' + facet.count + ')').
+            attr('for', 'fl-' + facet.name);
+            return $('<li>').append(checkbox).append(label);
+        });
+    });
+    $('#outcome-facet').html(function () {
+        allFacetValues = content.getFacetValues('outcome');
+        sortedFacetValues = allFacetValues.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));
+        return $.map(sortedFacetValues, function (facet) {
+            var checkbox = $('<input type=checkbox>').
+            data('facet', facet.name).
+            attr('id', 'fl-' + facet.name);
+            if (facet.isRefined) checkbox.attr('checked', 'checked');
+            var label = $('<label>').html(facet.name + ' (' + facet.count + ')').
+            attr('for', 'fl-' + facet.name);
+            return $('<li>').append(checkbox).append(label);
+        });
+    });
+    $('#difference-facet').html(function () {
+        allFacetValues = content.getFacetValues('difference');
+        sortedFacetValues = allFacetValues.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));
+        return $.map(sortedFacetValues, function (facet) {
             var checkbox = $('<input type=checkbox>').
             data('facet', facet.name).
             attr('id', 'fl-' + facet.name);
@@ -205,6 +299,22 @@ $('.section-display').on('change', function(){
         }
     });
     helper.search();
+});
+
+$('.facet-highlight').on('change', function(){
+    $('.facet-highlight:checkbox').each(function () {
+        var is_checked = this.checked;
+        if(!is_checked){
+            var type = $(this).val();
+            $('span.'+type).each(function () {
+                $(this).removeClass(type+'-pill')
+            });
+        }
+        else{
+            var type = $(this).val();
+            highlightFilter(type);
+        }
+    });
 });
 
 helper.search();
